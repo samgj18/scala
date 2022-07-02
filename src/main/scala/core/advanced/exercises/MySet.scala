d3bbba026237c17a86ebe7e8e49a384faa2b9ef1
samgj18 <samgj_18@hotmail.com>
2022-07-02 20:52:56 +0100

    More operations to the Set

---
+++ src/main/scala/core/advanced/exercises/MySet.scala
@@ -0,0 +1,90 @@
+package core.advanced.exercises
+
+import cats.instances.tailRec
+import scala.annotation.tailrec
+
+trait MySet[A] extends (A => Boolean) {
+  def apply(element: A): Boolean = contains(element)
+  def contains(elem: A): Boolean
+  def +(elem: A): MySet[A]
+  def ++(anotherSet: MySet[A]): MySet[A]
+
+  def map[B](f: A => B): MySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B]
+  def filter(predicate: A => Boolean): MySet[A]
+
+  def -(elem: A): MySet[A]
+  def --(anotherSet: MySet[A]): MySet[A]
+  def &(anotherSet: MySet[A]): MySet[A]
+}
+
+class EmptySet[A] extends MySet[A] {
+
+  self =>
+  override def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
+
+  override def filter(predicate: A => Boolean): MySet[A] = self
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
+
+  override def map[B](f: A => B): MySet[B] = new EmptySet[B]
+
+  override def +(elem: A): MySet[A] = new NonEmptySet[A](elem, self)
+
+  override def contains(elem: A): Boolean = false
+
+  override def -(elem: A): MySet[A] = self
+
+  override def --(anotherSet: MySet[A]): MySet[A] = self
+
+  override def &(anotherSet: MySet[A]): MySet[A] = self
+}
+
+class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
+
+  self =>
+  override def contains(elem: A): Boolean = elem == head || tail.contains(elem)
+
+  override def ++(anotherSet: MySet[A]): MySet[A] = tail ++ anotherSet + head
+
+  override def filter(predicate: A => Boolean): MySet[A] = {
+    val filtered = tail filter predicate
+    if (predicate(head)) filtered + head
+    else filtered
+  }
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] =
+    (tail flatMap f) ++ f(head)
+
+  override def map[B](f: A => B): MySet[B] = (tail map f) + f(head)
+
+  override def +(elem: A): MySet[A] =
+    if (self.contains(elem)) self else new NonEmptySet[A](elem, self)
+
+  override def -(elem: A): MySet[A] = self.filter(_ != elem)
+
+  override def --(anotherSet: MySet[A]): MySet[A] =
+    self.filter(x => !anotherSet(x))
+
+  override def &(anotherSet: MySet[A]): MySet[A] = self.filter(anotherSet)
+}
+
+object MySet {
+  def apply[A](values: A*): MySet[A] = {
+    @tailrec
+    def make(seq: Seq[A], acc: MySet[A]): MySet[A] = {
+      if (seq.isEmpty) acc
+      else make(seq.tail, acc + seq.head)
+    }
+
+    make(values.toSeq, new EmptySet())
+  }
+}
+
+object Playground extends App {
+  val s1 = MySet(1, 2, 3, 4)
+
+  val s2 = MySet(1, 2, 3, 4)
+
+  (s1 - 1 - 2 - 3 - 4) map println
+}
---
+++ src/main/scala/core/advanced/lectures/AdvancedFunctionalProgramming.scala
@@ -0,0 +1,77 @@
+package core.advanced.lectures
+
+object PartialFunctions extends App {
+  val aFunction = (x: Int) => x + 1 // Function1[Int, Int] === Int => Int
+
+  val aFussyFunction = (x: Int) =>
+    if (x == 1) 42
+    else if (x == 2) 56
+    else if (x == 5) 999
+    else throw new FunctionNotApplicableException
+
+  class FunctionNotApplicableException extends RuntimeException
+
+  val aNicerFussyFunction = (x: Int) =>
+    x match {
+      case 1 => 42
+      case 2 => 56
+      case 5 => 999
+    }
+
+  // {1,2,5} => Int
+  val aPartialFunction: PartialFunction[Int, Int] = {
+    case 1 => 42
+    case 2 => 56
+    case 5 => 999
+  } // partial function value
+
+  // PF utilities
+  println(aPartialFunction.isDefinedAt(67))
+
+  // lift
+  val lifted = aPartialFunction.lift // Int => Option[Int]
+  println(lifted(2)) // Some(56)
+  println(lifted(98)) // None
+
+  // chain
+  val pfChain = aPartialFunction.orElse[Int, Int] { case 45 =>
+    67
+  } // Int => Int
+
+  // PF extend normal functions
+  val aTotalFunction: Int => Int = { case 1 =>
+    99
+  }
+
+  // HOFs accept partial functions as well
+  val aMappedList = List(1, 2, 3).map {
+    case 1 => 42
+    case 2 => 78
+    case 3 => 1000
+  }
+
+  // PF can only have one parameter type
+
+  /*
+        1. construct partial functions
+        2. dumb chatbot
+   */
+  val manualFussyFunction = new PartialFunction[Int, Int] {
+    override def apply(x: Int): Int = x match {
+      case 1 => 42
+      case 2 => 65
+      case 5 => 999
+    }
+
+    override def isDefinedAt(x: Int): Boolean =
+      x == 1 || x == 2 || x == 5
+  }
+
+  val chatbot: PartialFunction[String, String] = {
+    case "hello"   => "Hi, my name is HAL9000"
+    case "goodbye" => "Once you start talking to me, there is no return, human!"
+    case "call mom" => "Unable to find your phone without your credit card"
+  }
+
+  scala.io.Source.stdin.getLines().foreach(line => println(chatbot(line)))
+}
