11ababe91a933ccbddea7465b596c516dca3677c
samgj18 <samgj_18@hotmail.com>
2023-07-16 22:11:53 +0100

    Starting akka journey

---
+++ src/main/scala/core/advanced/lectures/Multithreading.scala
@@ -0,0 +1,68 @@
+package core.advanced.lectures
+
+import scala.util.Success
+import scala.util.Failure
+import scala.concurrent.Promise
+
+object Multithreading extends App {
+  val aThread = new Thread(new Runnable {
+    override def run(): Unit = println("Running in parallel")
+  })
+
+  aThread.start()
+  aThread.join()
+
+  val threadHello = new Thread(() => (1 to 1000).foreach(_ => println("Hello")))
+  val threadGoodbye = new Thread(() =>
+    (1 to 1000).foreach(_ => println("Goodbye"))
+  )
+
+  threadHello.start()
+  threadGoodbye.start()
+
+  // different runs produce different results!
+
+  // Volatile does something similar to synchronized
+  // but only works for primitive types. For a deeper explanation
+  // check concurrent programming in scala book
+  class BankAccount(@volatile private var amount: Int) {
+    override def toString: String = "" + amount
+
+    def withdraw(money: Int): Unit = this.amount -= money
+
+    def safeWithdraw(money: Int): Unit = this.synchronized {
+      this.amount -= money
+    }
+  }
+
+  // inter-thread communication on the JVM
+  // wait - notify mechanism
+
+  // Scala Futures
+  // This is a ForkJoinPool under the hood
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Future
+
+  val aFuture = Future {
+    // long computation here
+    42
+  }
+
+  aFuture.onComplete {
+    case Success(value) => println(s"Got the callback, meaning = $value")
+    case Failure(exception) =>
+      println(s"Got the callback, exception = $exception")
+  }
+
+  // Promises complete Futures manually
+  // Promises are a "controller" over a Future
+  val aPromise = Promise[Int]()
+
+  val futureFromPromise = aPromise.success(42).future
+
+  futureFromPromise.onComplete {
+    case Success(value) => println(s"Got the callback, meaning = $value")
+    case Failure(exception) =>
+      println(s"Got the callback, exception = $exception")
+  }
+}
---
+++ src/main/scala/core/advanced/lectures/ThreadModelLimitation.scala
@@ -0,0 +1,76 @@
+package core.advanced.lectures
+
+object ThreadModelLimitation extends App {
+  // 1. OOP encapsulation is only valid in the SINGLE THREADED MODEL
+  class BankAccount(private var amount: Int) {
+    override def toString: String = "" + amount
+
+    def withdraw(money: Int): Unit = this.amount -= money
+
+    def safeWithdraw(money: Int): Unit = this.synchronized {
+      this.amount -= money
+    }
+
+    def deposit(money: Int): Unit = this.amount += money
+
+    def safeDeposit(money: Int): Unit = this.synchronized {
+      this.amount += money
+    }
+
+    def getAmount: Int = amount
+
+  }
+
+  val account = new BankAccount(2000)
+  for (_ <- 1 to 1000) {
+    new Thread(() => account.withdraw(1)).start()
+  }
+
+  for (_ <- 1 to 1000) {
+    new Thread(() => account.deposit(1)).start()
+  }
+
+  // This will NOT be 2000
+  println(account.getAmount)
+
+  // OOP encapsulation is broken in a multithreaded environment
+  // Synchronization! Locks to the rescue
+  //
+  // Deadlocks, livelocks, and starvation
+
+  // 2. Delegating something to a thread is a PAIN
+  // you have a running thread and you want to pass a runnable to that thread
+  var task: Runnable = null
+  val runningThread: Thread = new Thread(() => {
+    while (true) {
+      while (task == null) {
+        runningThread.synchronized {
+          println("[background] waiting for a task...")
+          runningThread.wait()
+        }
+      }
+
+      task.synchronized {
+        println("[background] I have a task")
+        task.run()
+        task = null
+      }
+    }
+  })
+
+  def delegateToBackgroundThread(r: Runnable): Unit = {
+    if (task == null) task = r
+
+    runningThread.synchronized {
+      runningThread.notify()
+    }
+  }
+
+  runningThread.start()
+  Thread.sleep(500)
+  delegateToBackgroundThread(() => println(42))
+  Thread.sleep(1000)
+  delegateToBackgroundThread(() => println("This should run in the background"))
+
+  // 3. Tracing and dealing with errors in a multithreaded environment is a PITN
+}
