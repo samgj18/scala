bdbd28cefa43acc574ddc2952b470dc15786e930
samgj18 <samgj_18@hotmail.com>
2023-09-18 22:46:51 +0100

    Add TestProbeSpec and TimeAssertionSpec classes to the lectures package.

---
+++ src/main/scala/actors/lectures/TestProbeSpec.scala
@@ -0,0 +1,76 @@
+package actors.lectures
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class TestProbeSpec
+    extends TestKit(ActorSystem("TestProbeSpec"))
+    with ImplicitSender
+    with AnyWordSpecLike
+    with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import TestProbeSpec._
+
+  "A master actor" should {
+    "register a slave" in {
+      val master = system.actorOf(Props[Master])
+      val slave  = TestProbe("slave")
+
+      master ! Register(slave.ref)
+
+      expectMsg(Ack)
+    }
+
+    "send the work to the slave actor" in {
+      val master = system.actorOf(Props[Master])
+      val slave  = TestProbe("slave")
+      master ! Register(slave.ref)
+      expectMsg(Ack)
+
+      val workload = "I love Akka"
+
+      master ! Work(workload)
+
+      slave.expectMsg(SlaveWork(workload, testActor))
+      // you can also mock the replies
+      slave.reply(Completed(3, testActor))
+
+      // Check receiveWhile
+      expectMsg(Report(3))
+    }
+  }
+}
+
+object TestProbeSpec {
+  case class Register(slave: ActorRef)
+  case class Work(text: String)
+  case class SlaveWork(text: String, requester: ActorRef)
+  case class Completed(count: Int, requester: ActorRef)
+  case class Report(count: Int)
+  case object Ack
+
+  class Master extends Actor {
+    override def receive: Receive = {
+      case Register(slave) =>
+        sender() ! Ack
+        context.become(online(slave, 0))
+      case _               =>
+    }
+
+    def online(ref: ActorRef, count: Int): Receive = {
+      case Work(text) =>
+        ref ! SlaveWork(text, sender())
+
+      case Completed(total, requester) =>
+        val newCount = count + total
+        requester ! Report(newCount)
+        context.become(online(ref, newCount))
+    }
+  }
+}
---
+++ src/main/scala/actors/lectures/TimeAssertionSpec.scala
@@ -0,0 +1,73 @@
+package actors.lectures
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import scala.concurrent.duration._
+import java.util.Random
+
+class TimeAssertionSpec
+    extends TestKit(ActorSystem("TestProbeSpec"))
+    with ImplicitSender
+    with AnyWordSpecLike
+    with BeforeAndAfterAll {
+
+  import TimeAssertionSpec._
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A worker actor" should {
+    val worker = system.actorOf(Props[WorkerActor])
+    "reply with the meaning of life in a timely manner" in {
+      within(500.millis, 1.second) { // Between 500 millis and at most 1 second
+        worker ! Work
+        expectMsg(WorkResult(42))
+      }
+    }
+
+    "reply in a reasonable cadence" in {
+      within(1.second) {
+        worker ! WorkSeq
+        val res = receiveWhile[Int](2.seconds, 500.millis, 10) { case WorkResult(res) =>
+          res
+        }
+
+        assert(res.sum > 5)
+      }
+    }
+
+    "reply to a test probe in a timely manner" in {
+      within(1.second) { 
+        val probe = TestProbe()
+        probe.send(worker, Work)
+        probe.expectMsg(WorkResult(42))
+      }
+    }
+  }
+
+  object TimeAssertionSpec {
+
+    case object Work
+    case object WorkSeq
+    case class WorkResult(res: Int)
+
+    class WorkerActor extends Actor {
+      override def receive: Receive = {
+        case Work    =>
+          // Long Computation
+          Thread.sleep(500)
+          sender() ! WorkResult(42)
+        case WorkSeq =>
+          val random = new Random()
+          (0 until 10).map { _ =>
+            Thread.sleep(random.nextInt(50))
+            sender() ! WorkResult(1)
+          }
+      }
+    }
+  }
+}
